22bea5755964cb8bb15157d1dc4be8f9e2895d84
4uhar1k <vovas0712@gmail.com>
2025-04-03 14:48:34 +0200

    created a page with followed courses

--- MyOwnCourseApp/MauiProgram.cs
+++ MyOwnCourseApp/MauiProgram.cs
@@ -22,11 +22,10 @@ namespace MyOwnCourseApp
             builder.Services.AddMOCApiClientService(x => x.ApiBaseAdress = "http://10.0.2.2:5000");
             builder.Services.AddTransient<IdToNameConverter>();
             builder.Services.AddTransient<MainPage>();
-
             builder.Services.AddTransient<SignUpNInViewModel>();
             builder.Services.AddTransient<CourseViewModel>();
             builder.Services.AddTransient<MyCoursesPage>();
-
+            builder.Services.AddTransient<FollowedCoursesPage>();
             builder.Services.AddSingleton<SqlConnectionBase>();
 
 #if DEBUG
---
+++ MyOwnCourseApp/Views/FollowedCoursesPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MyOwnCourseApp.Views;
+
+public partial class FollowedCoursesPage : ContentPage
+{
+	public FollowedCoursesPage()
+	{
+		InitializeComponent();
+	}
+}
