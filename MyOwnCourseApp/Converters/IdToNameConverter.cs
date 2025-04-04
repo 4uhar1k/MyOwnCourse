5c6c8d5bf11d0e8161b393bc116670e34f15bf1e
4uhar1k <vovas0712@gmail.com>
2025-04-04 14:01:25 +0200

    now author of a course is to see on coursereviewpage

--- MyOwnCourseApp/Converters/IdToNameConverter.cs
+++ MyOwnCourseApp/Converters/IdToNameConverter.cs
@@ -37,7 +37,17 @@ namespace MyOwnCourseApp.Converters
             return null;
         }
 
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
         public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
