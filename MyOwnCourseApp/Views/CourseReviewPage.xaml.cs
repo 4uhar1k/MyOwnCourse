1b08a3b74143a8def1f6c3d55b3b74134c9b8e56
4uhar1k <vovas0712@gmail.com>
2025-04-04 13:45:42 +0200

    created coursereviewpage

---
+++ MyOwnCourseApp/Views/CourseReviewPage.xaml.cs
@@ -0,0 +1,32 @@
+using MyOwnCourseApiClient;
+using MyOwnCourseApiClient.Models.ApiModels;
+using MyOwnCourseApp.ViewModels;
+
+namespace MyOwnCourseApp;
+
+public partial class CourseReviewPage : ContentPage
+{
+	private readonly MOCApiClientService _apiClient;
+    public Course curCourse { get; set; }
+    public CourseViewModel thisContext { get; set; }
+    public CourseReviewPage()
+	{
+		InitializeComponent();
+        _apiClient = null;
+        curCourse = new Course();
+        thisContext = new CourseViewModel(_apiClient);
+    }
+
+    public CourseReviewPage(MOCApiClientService apiClient, Course selectedCourse)
+    {        
+        InitializeComponent();
+        _apiClient = apiClient;
+        curCourse = selectedCourse;
+        thisContext = new CourseViewModel(_apiClient);
+        thisContext.Name = curCourse.Name;
+        thisContext.Category = curCourse.Category;
+        thisContext.Creator = curCourse.Creator;
+        thisContext.Status = thisContext.Status;
+        BindingContext = thisContext;
+    }
+}
